5b2c8f5eb7ab8c497b3ec98429dd81779b7cc87d
rakshithrs <rakshith@stridefuture.com>
2021-07-12 18:59:37 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log();
+console.log();
+console.log();
+
+console.log("Author   : rrsalian16");
+console.log("Name     : Rakshith R Salian");
+console.log("Website  : https://www.rrsalian16.com");
+console.log("GitHub   : https://github.com/rrsalian16");
+console.log("Twitter  : https://twitter.com/rrsalian16");
+console.log("LinkedIn : https://www.linkedin.com/in/rrsalian16/");
+console.log("Email    : rrsalian16@gmail.com");
+
+console.log();
+console.log();
+console.log();
