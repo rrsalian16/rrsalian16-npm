f962bf0fa66dd77b20a199aca2b8ff99a74564e2
rakshithrs <rakshith@stridefuture.com>
2021-07-12 19:15:45 +0530

    npx rrsalian16.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-console.log();
-console.log();
-console.log();
+#!/usr/bin/env node
 
-console.log("Author   : rrsalian16");
-console.log("Name     : Rakshith R Salian");
-console.log("Website  : https://www.rrsalian16.com");
-console.log("GitHub   : https://github.com/rrsalian16");
-console.log("Twitter  : https://twitter.com/rrsalian16");
-console.log("LinkedIn : https://www.linkedin.com/in/rrsalian16/");
-console.log("Email    : rrsalian16@gmail.com");
+console.log(`
 
-console.log();
-console.log();
-console.log();
+
+Author   : rrsalian16
+Name     : Rakshith R Salian
+Website  : https://www.rrsalian16.com
+GitHub   : https://github.com/rrsalian16
+Twitter  : https://twitter.com/rrsalian16
+LinkedIn : https://www.linkedin.com/in/rrsalian16/
+Email    : rrsalian16@gmail.com
+
+
+`);
